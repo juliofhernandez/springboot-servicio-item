e672ad27a014318f0b7aacb733ba649f80400aec
juliofhernandez <juliofernando1c@gmail.com>
2023-08-23 21:22:04 +0200

    Resilience4J | circuitBreakerFactory

--- src/main/java/com/microservices/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/microservices/springboot/app/item/controllers/ItemController.java
@@ -1,8 +1,12 @@
 package com.microservices.springboot.app.item.controllers;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservices.springboot.app.item.models.Item;
 import com.microservices.springboot.app.item.models.Product;
 import com.microservices.springboot.app.item.models.service.ItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
+	
+	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private CircuitBreakerFactory circuitBreakerFactory;
 	
 	@Autowired
 //	@Qualifier("itemServiceImplFeign")
@@ -28,13 +36,14 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
-	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/items/{id}/quantity/{quantity}")
 	public Item findById(@PathVariable Long id, @PathVariable Integer quantity) {
-		return itemService.findById(id, quantity);
+//		return itemService.findById(id, quantity);
+		return circuitBreakerFactory.create("items").run(() -> itemService.findById(id, quantity), e -> metodoAlternativo(id, quantity, e));
 	}
 	
-	public Item metodoAlternativo(Long id, Integer quantity) {
+	public Item metodoAlternativo(Long id, Integer quantity, Throwable throwable) {
+		logger.info(throwable.getMessage());
 		Item item = new Item();
 		Product product = new Product();
 		product.setId(id);
